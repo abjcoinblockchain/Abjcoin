62c1c4b8b53a69b2a8275d67ed3740fb65f32fc3
btcltcdigger <btcltcdigger@gmail.com>
2017-11-07 18:17:23 +0000

    code change

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define AbjCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "AbjCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
